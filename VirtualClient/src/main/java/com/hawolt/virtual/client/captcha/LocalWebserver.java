864ef0645ca6cc828d4f8121c643071adc63e2ce
hawolt <45050975+hawolt@users.noreply.github.com>
2023-08-28 23:33:01 +0200

    switch to utility browse method from network

--- VirtualClient/src/main/java/com/hawolt/virtual/client/captcha/LocalWebserver.java
+++ VirtualClient/src/main/java/com/hawolt/virtual/client/captcha/LocalWebserver.java
@@ -1,5 +1,6 @@
 package com.hawolt.virtual.client.captcha;
 
+import com.hawolt.generic.util.Network;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
@@ -41,18 +42,6 @@ public class LocalWebserver {
 
     public static void show(P1Callback callback) throws IOException {
         LocalWebserver.callback = callback;
-        browse("http://127.0.0.1:42069");
-    }
-
-    private static void browse(String url) throws IOException {
-        String os = System.getProperty("os.name").toLowerCase();
-        Runtime rt = Runtime.getRuntime();
-        if (os.contains("mac")) {
-            rt.exec("open " + url);
-        } else if (os.contains("nix") || os.contains("nux")) {
-            rt.exec(new String[]{"xdg-open", url});
-        } else {
-            rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
-        }
+        Network.browse("http://127.0.0.1:42069");
     }
 }
