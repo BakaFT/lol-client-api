2224fd2d9af7d7a91cab0e883559ee19223a3409
hawolt <45050975+hawolt@users.noreply.github.com>
2023-08-29 11:03:04 +0200

    allow file pre-caching & various utility methods

--- Manifest/src/main/java/com/hawolt/manifest/RMANCache.java
+++ Manifest/src/main/java/com/hawolt/manifest/RMANCache.java
@@ -5,8 +5,12 @@ import com.hawolt.logger.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created: 13/01/2023 16:45
@@ -15,41 +19,85 @@ import java.util.Set;
 
 public class RMANCache {
     private static final Path path = Paths.get(System.getProperty("java.io.tmpdir")).resolve("client-rman-cache");
-    private static final Set<String> cache = new HashSet<>();
+    private static final Map<String, byte[]> storage = new HashMap<>();
+    private static final Set<String> accessed = new HashSet<>();
 
-    static {
+    public static void preload() {
+        ExecutorService service = Executors.newSingleThreadExecutor();
         try {
             Files.createDirectories(path);
             File[] files = path.toFile().listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    cache.add(file.getName());
-                }
+            if (files == null) return;
+            for (File file : files) {
+                service.execute(() -> {
+                    try {
+                        loadToMemory(file.getName());
+                    } catch (IOException e) {
+                        Logger.error(e);
+                    }
+                });
             }
         } catch (IOException e) {
             Logger.debug("Unable to create cache directory");
         }
+        service.shutdown();
+    }
+
+    public static void cleanup() {
+        clearMemoryMapping();
+        purge();
+    }
+
+    public static void purge() {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        Set<String> names = getUsedFiles();
+        service.execute(() -> {
+            File[] files = path.toFile().listFiles();
+            if (files == null) return;
+            for (File file : files) {
+                if (!names.contains(file.getName()) && file.delete()) {
+                    Logger.debug("[rman-cache] remove old file from cache cache: {}", file.getName());
+                }
+            }
+        });
+        service.shutdown();
+    }
+
+    public static Set<String> getUsedFiles() {
+        return accessed;
+    }
+
+    public static void clearMemoryMapping() {
+        storage.replaceAll((k, v) -> new byte[0]);
     }
 
     public static boolean isCached(String name) {
-        return cache.contains(name);
+        return storage.containsKey(name);
+    }
+
+    private static byte[] loadToMemory(String name) throws IOException {
+        byte[] bytes = Files.readAllBytes(path.resolve(name));
+        storage.put(name, bytes);
+        return bytes;
     }
 
     public static byte[] load(String name) throws IOException {
-        return Files.readAllBytes(path.resolve(name));
+        accessed.add(name);
+        if (storage.containsKey(name)) return storage.get(name);
+        return loadToMemory(name);
     }
 
     public static void store(String name, Path origin) throws IOException {
         if (!path.toFile().exists()) return;
         Logger.debug("[rman-cache] storing file in cache: {}", name);
         Files.move(origin, path.resolve(name), StandardCopyOption.REPLACE_EXISTING);
-        cache.add(name);
+        load(name);
     }
 
     public static void store(String name, byte[] b) throws IOException {
         if (!path.toFile().exists()) return;
         Logger.debug("[rman-cache] storing file in cache: {}", name);
         Files.write(path.resolve(name), b, StandardOpenOption.CREATE);
-        cache.add(name);
+        load(name);
     }
 }
